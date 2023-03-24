de1ce571f67ceb98d5aac25034303ec6613c21d3
Rayden Sia <85201523+raydensia@users.noreply.github.com>
2023-03-24 16:00:02 +0800

    add media queries, change grid to use grid mode determined that lag is caused by grid borders, not code implementation. learned not to use vh or vw, getting a better idea on how to position items for use on different devices.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -12,8 +12,8 @@ const gridSizeText = document.querySelector('#gridSizeText');
 gridSizeText.textContent = `${gridSlider.value} x ${gridSlider.value}`;
 gridSlider.addEventListener("input", (event) => {
     gridSizeText.textContent = `${event.target.value} x ${event.target.value}`;
-    initiateGrid();
 })
+gridSlider.addEventListener("mouseup", () => createGrid(`${gridSlider.value}`, gridState));
 
 const colorBtn = document.getElementById('colorBtn');
 const randomBtn = document.getElementById('randomBtn');
@@ -21,6 +21,7 @@ const eraserBtn = document.getElementById('eraserBtn');
 const clearBtn = document.getElementById('clearBtn');
 const gridBtn = document.getElementById('gridBtn');
 const colorPicker = document.getElementById('colorPicker');
+const grid = document.getElementById('etch-a-sketch');
 
 colorBtn.onclick = () => {
     currentMode = 'color';
@@ -34,48 +35,34 @@ eraserBtn.onclick = () => {
     currentMode = 'eraser';
     activateButton(currentMode);
 }
-clearBtn.onclick = () => initiateGrid();
+clearBtn.onclick = () => createGrid(`${gridSlider.value}`, gridState);
 gridBtn.onclick = () => toggleGrid();
 
 
-function createGrid(size, grid=true) {
-    let gridContainer = document.createElement("div");
-    let etchASketch = document.querySelector("#etch-a-sketch");
-    if (etchASketch.hasChildNodes()) {
-        etchASketch.innerHTML = "";
-    }
-    gridContainer.style.maxWidth = `${etchASketch.clientWidth}px`;
-    gridContainer.style.maxHeight = `${etchASketch.clientHeight}px`;
-    gridContainer.style.border = "0";
-
-    for (let i = 0; i < size; i++) {
-        let gridRow = document.createElement("div");
-        gridRow.style.display = "flex";
-        gridRow.style.maxWidth = `${etchASketch.clientWidth}px`;
-        gridRow.style.minWidth = `${etchASketch.clientWidth}px`;
-        gridRow.style.border = "0";
-        for (let j = 0; j < size; j++) {
-            let gridSquare = document.createElement("div");
-            gridSquare.className = "gridSquare";
-            gridSquare.style.minWidth = `${etchASketch.clientWidth / size}px`;
-            gridSquare.style.minHeight = `${etchASketch.clientHeight / size}px`;
-            if (grid) {
-                gridSquare.style.border = "dotted 1px";
-                gridSquare.style.borderColor = "rgba(0, 0, 0, 0.2)";
-            }
-            else {
-                gridSquare.style.border = "none";
-            }
-            gridRow.appendChild(gridSquare);
+function createGrid(size, gridstate=true) {
+    grid.innerHTML = '';
+    grid.style.gridTemplateColumns = `repeat(${gridSlider.value}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${gridSlider.value}, 1fr)` 
+    
+    for (let i = 0; i < size * size; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('gridSquare');
+        if (gridstate) {
+            gridSquare.style.border = "dotted 1px";
+            gridSquare.style.borderColor = "rgba(0, 0, 0, 0.2)";
         }
-        gridContainer.appendChild(gridRow);
+        else {
+            gridSquare.style.border = "none";
+        }
+        gridSquare.addEventListener('mouseover', changeColor);
+        gridSquare.addEventListener('mousedown', changeColor);
+        grid.appendChild(gridSquare);
     }
-    etchASketch.appendChild(gridContainer);
 }
 
 function toggleGrid() {
     gridState = !gridState;
-    initiateGrid();
+    createGrid(`${gridSlider.value}`, gridState);
     if (gridState) {
         document.getElementById("gridBtn").textContent = "Hide Grid";
     }
@@ -120,17 +107,9 @@ function activateButton(currentMode) {
     }
 }
 
-function initiateGrid() {
-    createGrid(`${gridSlider.value}`, gridState);
-    const gridSquares = document.querySelectorAll('.gridSquare');
-    gridSquares.forEach(gridSquare => {
-        gridSquare.addEventListener('mousedown', changeColor);
-        gridSquare.addEventListener('mouseover', changeColor);
-    })
-}
 
 
-initiateGrid();
+createGrid(`${gridSlider.value}`, gridState);
 currentMode = 'color';
 activateButton(currentMode);
 
